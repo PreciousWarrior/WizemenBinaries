0f5ea22e5705b1df20fee2c347cb029dfae162cd
PreciousWarrior / IamPrecious <61111041+PreciousWarrior@users.noreply.github.com>
2021-09-20 18:33:59 +0530

    figuring stuff out

--- src/update-mac.js
+++ src/update-mac.js
@@ -2,6 +2,4 @@ const fs = require('fs/promises');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-fs.writeFile('./nice.txt', 'nice!').then(async (_) => {
-	process.exit(0);
-});
+fs.writeFile('./nice.txt', 'nicer!');
